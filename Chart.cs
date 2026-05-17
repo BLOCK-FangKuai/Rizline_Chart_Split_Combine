e7a6b9c3c12d65eaf867ff03c6eb7587116736af
BLOCK-FangKuai <2861559104@qq.com>
2026-05-18 01:17:20 +0800

    添加项目文件。

---
+++ Chart.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rizline_Chart
+{
+    public class Chart
+    {
+        public int fileVersion;
+        public int chartDelayMs;
+        public List<Theme> themes;
+        public List<ChallengeTime> challengeTimes;
+        public float bPM;
+        public List<KeyPoint> bpmShifts;
+        public List<Line>? lines;
+        public List<CanvasMove> canvasMoves;
+        public List<CameraMove> cameraMoves;
+        public Db? __db;
+    }
+
+    public class Theme
+    {
+        /// <summary>
+        /// 第一个值表示背景色
+        /// 第二个值表示音符颜色
+        /// 第三个值表示UI颜色
+        /// </summary>
+        public Color[] colorsList = new Color[3];
+    }
+
+    public class Color
+    {
+        public int r, g, b, a;
+    }
+
+    public class ChallengeTime
+    {
+        public float checkPoint, start, end, transTime;
+    }
+
+    public class Line
+    {
+        public List<LinePoint> linePoints;
+        public List<Note> notes;
+        public List<ColorKeyPoint> judgeRingColor;
+        public List<ColorKeyPoint> lineColor;
+    }
+
+    public class LinePoint
+    {
+        public float time;
+        public float xPosition;
+        public Color color;
+        public int easeType;
+        public int canvasIndex;
+        public float floorPosition;
+    }
+
+    public class Note
+    {
+        public int type;
+        public float time;
+        public float floorPosition;
+        /// <summary>
+        /// Hold音符特有的参数
+        /// 第一个数表示结束的时间
+        /// 第二个数表示Hold尾所在画布的index
+        /// 第三个数表示Hold尾在所在画布的floorPosition
+        /// </summary>
+        public float[] otherInformations = new float[3];
+    }
+
+    public class ColorKeyPoint
+    {
+        public Color startColor;
+        public Color endColor;
+        public float time;
+    }
+
+    public class CanvasMove
+    {
+        public int index;
+        public List<KeyPoint> xPositionKeyPoints;
+        public List<KeyPoint> speedKeyPoints;
+    }
+
+    public class CameraMove
+    {
+        public List<KeyPoint> scaleKeyPoints;
+        public List<KeyPoint> xPositionKeyPoints;
+    }
+
+    public class KeyPoint
+    {
+        public float time, value;
+        public int easeType;
+        public float floorPosition;
+    }
+
+    public class Db
+    {
+        public string steam_workshop_fileid,
+            steam_workshop_userid,
+            steam_workshop_title,
+            steam_workshop_description,
+            steam_workshop_composer,
+            steam_workshop_difficulty,
+            steam_workshop_level,
+            steam_workshop_previewtime,
+            steam_workshop_illustration,
+            steam_workshop_visibility;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+
+namespace Rizline_Chart
+{
+    public class Program
+    {
+        static string directory;
+        static List<string> fileNames;
+        //此处记录的是节拍
+        static List<float> splitTime = new();
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                directory = PyInput("请输入json文件目录：", "charts");
+                if (!Directory.Exists(directory))
+                {
+                    directory = Path.Combine(Directory.GetCurrentDirectory(), directory);
+                    if (Directory.Exists(directory))
+                    {
+                        break;
+                    }
+                    Pause($"目录{directory}不存在，请重新输入");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            fileNames = GetAllFiles(directory);
+            PyPrint($"选择目录：{Path.GetFullPath(directory)}");
+
+            PyPrint("是否使用自动导入？（Y/N）");
+
+            while (true)
+            {
+                var choose = Console.ReadKey(true);
+                if ($"{choose.KeyChar}".ToUpper().Equals("Y"))
+                {
+                    AutoImport();
+                    break;
+                }
+                else if ($"{choose.KeyChar}".ToUpper().Equals("N"))
+                {
+                    ManualImport();
+                    break;
+                }
+            }
+
+            
+            //string json = File.ReadAllText("D:\\Codes\\Rizline_Chart\\Test\\Research_Json.json");
+            //Chart? chart = JsonConvert.DeserializeObject<Chart>(json);
+            Pause();
+        }
+
+        private static List<string> GetAllFiles(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+                return [.. files];
+            }
+            else
+            {
+                throw new DirectoryNotFoundException(path);
+            }
+        }
+
+        private static void AutoImport()
+        {
+            try
+            {
+                fileNames.Sort((name1, name2) =>
+                {
+                    return int.Parse(name1.Split(".")[0]).CompareTo(int.Parse(name2.Split(".")[0]));
+                });
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static void ManualImport()
+        {
+
+        }
+
+        private static string PyInput(string message = "", string @default = "")
+        {
+            Console.WriteLine(message);
+            string? read = Console.ReadLine();
+            string result = string.IsNullOrWhiteSpace(read) ? @default : read;
+            return result;
+        }
+
+        private static void PyPrint(string message = "") {  Console.WriteLine(message); }
+
+        private static void Pause(string message = "")
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("按任意键继续……");
+            Console.ReadKey();
+        }
+    }
+}
