1badaf075ac80250c3407f6605d617efb56ae1fc
BLOCK-FangKuai <2861559104@qq.com>
2026-07-08 02:55:31 +0800

    完善Settings，添加获取配置文件与基础谱面的方法，添加可能存在的异常错误提示

--- Program.cs
+++ Program.cs
@@ -7,66 +7,134 @@ namespace Rizline_Chart
     {
         static string directory;
         static List<string> filePaths;
-        //此处记录的是节拍
-        static List<float> splitTimes = new();
+        static Settings settings;
+        static Chart resultChart = new();
+
+        static JsonSerializerSettings serializerSettings = new()
+        {
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+            Formatting = Formatting.Indented,
+            NullValueHandling = NullValueHandling.Ignore
+        };
 
         public static void Main(string[] args)
         {
             while (true)
             {
-                directory = PyInput("请输入json文件目录：", "charts");
-                if (!Directory.Exists(directory))
+                try
                 {
-                    directory = Path.Combine(Directory.GetCurrentDirectory(), directory);
-                    if (Directory.Exists(directory))
+                    while (true)
                     {
-                        break;
+                        directory = PyInput("请输入json文件目录：", "charts");
+                        if (!Directory.Exists(directory))
+                        {
+                            directory = Path.Combine(Directory.GetCurrentDirectory(), directory);
+                            if (Directory.Exists(directory))
+                            {
+                                break;
+                            }
+                            Pause($"目录{directory}不存在，请重新输入");
+                        }
+                        else
+                        {
+                            break;
+                        }
                     }
-                    Pause($"目录{directory}不存在，请重新输入");
+
+                    PyPrint($"选择目录：{Path.GetFullPath(directory)}");
+
+                    PyPrint("是否使用自动导入？（Y/N）");
+
+                    while (true)
+                    {
+                        var choose = Console.ReadKey(true);
+                        if ($"{choose.KeyChar}".ToUpper().Equals("Y"))
+                        {
+                            AutoImport();
+                            //PyPrint(JsonConvert.SerializeObject(resultChart, serializerSettings));
+                            break;
+                        }
+                        else if ($"{choose.KeyChar}".ToUpper().Equals("N"))
+                        {
+                            ManualImport();
+                            break;
+                        }
+                    }
+
+                    // D:\Codes\Rizline_Chart\Test\Pastel Lines
+
+                    //string json = File.ReadAllText("D:\\Codes\\Rizline_Chart\\Test\\Research_Json.json");
+                    //Chart? chart = JsonConvert.DeserializeObject<Chart>(json);
+                    Pause();
                 }
-                else
+                catch (Exception e)
                 {
-                    break;
+                    PyPrint(e.Message);
+                    Pause();
+                    Console.Clear();
                 }
             }
-            
-            PyPrint($"选择目录：{Path.GetFullPath(directory)}");
-
-            PyPrint("是否使用自动导入？（Y/N）");
-
-            while (true)
-            {
-                var choose = Console.ReadKey(true);
-                if ($"{choose.KeyChar}".ToUpper().Equals("Y"))
-                {
-                    AutoImport();
-                    break;
-                }
-                else if ($"{choose.KeyChar}".ToUpper().Equals("N"))
-                {
-                    ManualImport();
-                    break;
-                }
-            }
-
-            
-            //string json = File.ReadAllText("D:\\Codes\\Rizline_Chart\\Test\\Research_Json.json");
-            //Chart? chart = JsonConvert.DeserializeObject<Chart>(json);
-            Pause();
         }
 
         private static void AutoImport()
         {
             filePaths = GetAllFiles(directory);
+            GetSettings(filePaths);
+            GetBaseChart();
             List<string> chartFiles = GetChartFiles();
             SortChartFiles(chartFiles);
         }
 
         private static void GetSettings(List<string> filePaths)
         {
-            if (filePaths.Contains(Path.Combine(directory, "settings.json")))
+            string settingPath = Path.Combine(directory, "settings.json");
+            if (filePaths.Contains(settingPath))
             {
+                try
+                {
+                    string json = File.ReadAllText(settingPath);
+                    settings = JsonConvert.DeserializeObject<Settings>(json);
+                    if (!settings.Check())
+                    {
+                        throw new Exception($"配置文件参数不正确：{settingPath}");
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException($"未找到配置文件：{settingPath}");
+            }
+        }
 
+        private static void GetBaseChart()
+        {
+            string baseChartPath = Path.Combine(directory, $"{settings.baseChartName}.json");
+            if (filePaths.Contains(baseChartPath))
+            {
+                try
+                {
+                    string json = File.ReadAllText(baseChartPath);
+                    Chart baseChart = JsonConvert.DeserializeObject<Chart>(json);
+
+                    resultChart.fileVersion = baseChart.fileVersion;
+                    resultChart.chartDelayMs = baseChart.chartDelayMs;
+                    resultChart.themes = baseChart.themes;
+                    resultChart.challengeTimes = baseChart.challengeTimes;
+                    resultChart.bPM = baseChart.bPM;
+                    resultChart.bpmShifts = baseChart.bpmShifts;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException($"未找到基础谱面文件：{baseChartPath}");
             }
         }
 
@@ -86,14 +154,35 @@ namespace Rizline_Chart
         private static List<string> GetChartFiles()
         {
             List<string> chartFiles = new();
-            foreach (var filePath in filePaths)
+            if (settings.IsFileEmpty())
             {
-                string fileName = Path.GetFileName(filePath);
-                //文件名带有数字编号且后缀为.json的计为谱面文件
-                bool isChartFile = int.TryParse(fileName.Split(".")[0], out _) && Path.GetExtension(filePath).ToLower().Equals("json");
-                if (isChartFile)
+                //没有指定哪些是谱面文件时
+                foreach (var filePath in filePaths)
                 {
-                    chartFiles.Add(fileName);
+                    string fileName = Path.GetFileName(filePath);
+                    //文件名带有数字编号且后缀为.json的计为谱面文件
+                    bool isChartFile = int.TryParse(fileName.Split(".")[0], out _) && Path.GetExtension(filePath).ToLower().Equals("json");
+                    if (isChartFile)
+                    {
+                        chartFiles.Add(fileName);
+                    }
+                }
+            }
+            else
+            {
+                //指定哪些是谱面文件时
+                foreach (var file in settings.files)
+                {
+                    string filePath = Path.Combine(directory, file);
+                    if (filePaths.Contains(filePath))
+                    {
+                        string fileName = Path.GetFileName(filePath);
+                        chartFiles.Add(fileName);
+                    }
+                    else
+                    {
+                        throw new FileNotFoundException($"未找到谱面文件：{filePath}");
+                    }
                 }
             }
             return chartFiles;
@@ -101,6 +190,11 @@ namespace Rizline_Chart
 
         private static void SortChartFiles(in List<string> chartFiles)
         {
+            //指定谱面文件后不再进行排序
+            if (!settings.IsFileEmpty())
+            {
+                return;
+            }
             chartFiles.Sort((name1, name2) =>
             {
                 //根据编号大小排序
--- Settings.cs
+++ Settings.cs
@@ -4,6 +4,22 @@ namespace Rizline_Chart
 {
     public class Settings
     {
-        public List<float> splitTimes;
+        //此处记录的是节拍
+        public List<float> splitTimes = new();
+        public float overlapTime = 4;
+        public string baseChartName = "base";
+        public List<string>? files;
+        public string resultName = "result";
+        public bool autoOverride = false;
+
+        public bool Check()
+        {
+            return IsFileEmpty() || splitTimes.Count + 1 == files.Count;
+        }
+
+        public bool IsFileEmpty()
+        {
+            return files == null || files.Count == 0;
+        }
     }
 }
